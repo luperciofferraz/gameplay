3d15fff0548a80e7c017ec650e44268a44a820eb
luperciofferraz <lupercio.ferraz@gmail.com>
2021-06-21 14:42:33 -0300

    Correction

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -11,10 +11,10 @@ export const styles = StyleSheet.create({
         borderRadius: 8,
         flexDirection: 'row',
         alignItems: 'center'
-
     },
 
     title: {
+
         flex: 1,
         color: theme.colors.heading,
         fontSize: 15,
