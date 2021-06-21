c77a78cdc3a8def4222fc4aaf21916f691d60bfe
luperciofferraz <lupercio.ferraz@gmail.com>
2021-06-21 01:33:18 -0300

    Login Page - Part One

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StatusBar } from 'react-native';
 import { styles } from './styles';
+
+import IllustrationImg from '../../assets/illustration.png';
+
 
 export function SignIn() {
 
@@ -8,7 +11,38 @@ export function SignIn() {
 
         <View style={styles.container}>
 
-            <Text>Hello World, NLW Together</Text>
+            <StatusBar 
+            
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            
+            />
+
+            <Image 
+                source={IllustrationImg} 
+                style={styles.image}
+                resizeMode="stretch"
+            />
+
+            <View style={styles.content}>
+
+                <Text style={styles.title}>
+
+                    Organize {'\n'}
+                    suas jogatinas  {'\n'}
+                    facilmente
+
+                </Text>
+
+                <Text style={styles.subtitle}>
+
+                    Crie grupos para jogar seus games {'\n'}
+                    favoritos com seus amigos
+
+                </Text>
+
+            </View>
 
         </View>
 
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,5 +1,7 @@
 import { StyleSheet } from 'react-native';
 
+import { theme } from '../../global/styles/theme';
+
 export const styles = StyleSheet.create({
 
     container: {
@@ -7,7 +9,34 @@ export const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: theme.colors.background
+    },
 
+    image: {
+
+        width: '100%',
+        height: 360
+    },
+
+    content: {
+        
+        marginTop: -40,
+    },
+
+    title: {
+        
+        color: theme.colors.heading,
+        textAlign: 'center',
+        fontSize: 40,
+        marginBottom: 16,
+    },
+
+    subtitle: {
+        
+        color: theme.colors.heading,
+        fontSize: 15,
+        textAlign: 'center',
+        marginBottom: 64
     }
 
 });
