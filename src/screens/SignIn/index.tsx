290226704c19cf566e05bda037ba7f0db4d0ef73
luperciofferraz <lupercio.ferraz@gmail.com>
2021-06-21 00:49:30 -0300

    Initial Project

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from './styles';
+
+export function SignIn() {
+
+    return (
+
+        <View style={styles.container}>
+
+            <Text>Hello World, NLW Together</Text>
+
+        </View>
+
+    )
+
+}
