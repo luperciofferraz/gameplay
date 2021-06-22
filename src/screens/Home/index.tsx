001a37d9ef6fddeb3776519766009108b203775a
luperciofferraz <lupercio.ferraz@gmail.com>
2021-06-22 15:35:07 -0300

    Home - Final

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,47 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View , FlatList } from 'react-native';
 import { styles } from './styles';
 
 import { CategorySelect } from './../../components/CategorySelect';
 import { Profile } from './../../components/Profile';
 import { ButtonAdd } from './../../components/ButtonAdd';
+import { ListDivider } from './../../components/ListDivider';
+import { ListHeader } from './../../components/ListHeader';
+import { Appointment } from './../../components/Appointment';
 
 export function Home() {
 
     const [category, setCategory] = useState('');
+
+    const appointments = [
+        
+        {
+            id: '1',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '22/06 às 20:40h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+        },
+
+        {
+            id: '2',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '22/06 às 20:40h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+        }
+
+    ]
 
     function handleCategorySelect(categoryId: string) {
         categoryId === category ? setCategory('') : setCategory(categoryId);
@@ -19,32 +52,34 @@ export function Home() {
         <View>
 
             <View style={styles.header}>
-                
                 <Profile />
-                
                 <ButtonAdd />
-
             </View>
 
-            <View>
+            <CategorySelect 
+                categorySelected={category}
+                setCategory={handleCategorySelect}
+            />
 
-                <CategorySelect 
-                
-                    categorySelected={category}
-                    setCategory={handleCategorySelect}
-                
+            <View style={styles.content}>
+                <ListHeader 
+                    title="Partidas Agendadas" 
+                    subtitle="Total 6"  
                 />
-
-                <View style={styles.content}>
-
-                    
-
-                </View>
-
+                <FlatList 
+                    data={appointments}
+                    keyExtractor={item => item.id}
+                    style={styles.matches}
+                    showsVerticalScrollIndicator={false}
+                    ItemSeparatorComponent={() => <ListDivider />}
+                    renderItem={({ item }) => (
+                        
+                        <Appointment 
+                            data={item}
+                        />
+                    )} 
+                />
             </View>
-
         </View>
-
     );
-
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -19,6 +19,11 @@ export const styles = StyleSheet.create({
 
     content: {
         marginTop: 42,
+    },
+
+    matches: {
+        marginTop: 24,
+        marginLeft: 24
     }
 
 })
